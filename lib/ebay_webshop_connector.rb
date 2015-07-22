17c3b6a7c9dd5d1784321a78f91aa4b1141271ee
Martin Eismann <martin@eismann.cc>
2015-07-22 11:51:31 +0200

    Add EbayWebshopConnector#get_ebay_official_time

--- lib/ebay_webshop_connector.rb
+++ lib/ebay_webshop_connector.rb
@@ -1,12 +1,26 @@
 require "ebay_webshop_connector/version"
 require 'ebayr'
 require 'psych'
+# extend Time with parsing methods
+require 'time'
 
 class EbayWebshopConnector
 
   def initialize(config_file)
     read_in(config_file).each do |key, value|
       Ebayr.send "#{key}=", value
+    end
+  end
+
+  def get_ebay_official_time
+    result = Ebayr.call :GeteBayOfficialTime
+
+    if result[:ack] == 'Success'
+      return Time.parse result[:timestamp]
+    end
+
+    if result[:errors]
+      raise EbayWebshopConnector::RetrievalError.new result[:errors]
     end
   end
 
@@ -17,3 +31,5 @@ class EbayWebshopConnector
   end
 
 end
+
+class EbayWebshopConnector::RetrievalError < StandardError; end
--- spec/ebay_webshop_connector_spec.rb
+++ spec/ebay_webshop_connector_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe EbayWebshopConnector do
-  ebay_spec_conf_file = '/tmp/ebay.spec_conf.yml'
+  ebay_dummy_conf_file = '/tmp/ebay.spec_conf.yml'
+  ebay_live_conf_file = 'ebay.conf.yml'
+
+  let(:config_file) { ebay_dummy_conf_file }
 
   before(:all) do
     config = Psych.dump(
@@ -12,11 +15,11 @@ describe EbayWebshopConnector do
       cert_id: "my-ebay-cert-id",
       ru_name: "my-ebay-ru-name")
 
-    File.write ebay_spec_conf_file, config
+    File.write ebay_dummy_conf_file, config
   end
 
   subject do
-    EbayWebshopConnector.new ebay_spec_conf_file
+    EbayWebshopConnector.new config_file
   end
 
   it 'has a version number' do
@@ -26,5 +29,36 @@ describe EbayWebshopConnector do
   it 'configures its dependency' do
     subject # initialise subject
     expect(Ebayr.dev_id).to eq 'my-dev-id'
+  end
+
+  unless File.exists? ebay_live_conf_file
+    puts <<-ERR
+    You may want to add a live Ebay sandbox configuration file
+    according to this sample in order to run more tests:
+
+    #{Psych.load_file(ebay_dummy_conf_file).to_yaml}
+    ERR
+  else
+
+    describe '#get_ebay_official_time' do
+      context 'with correct live Ebay sandbox authorisation' do
+        let(:config_file) { ebay_live_conf_file }
+
+        it 'retrieves the official Ebay time' do
+          time = subject.get_ebay_official_time
+          # Make sure your machines clock is not too wrong ;-)
+          expect(time).to be_within(5.minutes).of Time.new
+        end
+      end
+
+      context 'with incorrect live Ebay sandbox authorisation' do
+        let(:config_file) { ebay_dummy_conf_file }
+
+        it 'raises an EbayWebshopConnector::RetrievalError' do
+          expect{ subject.get_ebay_official_time }.to raise_error EbayWebshopConnector::RetrievalError
+        end
+      end
+
+    end
   end
 end
